0dcce450142629e801af178d525f2a768208d028
Alexsaphir <alex.game@sfr.fr>
2018-05-06 15:03:55 +0200

    fix error in the command

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
-reset && gfortran src/SKYLINE.f90 src/CHOLESKY.f90 src/SOLVER.f90 src/main.f90 $$ ./a.out 
+reset && gfortran src/SKYLINE.f90 src/CHOLESKY.f90 src/SOLVER.f90 src/main.f90 && ./a.out 
 
